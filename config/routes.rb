bc4b9a0ec3a70698d4d6199d1021b1fb93a63f88
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-09-29 15:41:48 +0100

    Index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   mount ReactiveRecord::Engine => '/rr'
-  get "/chat" => "chat#app"
+  get "/chat" => "chat#app" # This is broken
 
+  get "/" => "home#index"
   get "/show" => "home#show"
   get "/clock" => "home#clock"
   get "/unit_converter" => "home#unit_converter"
