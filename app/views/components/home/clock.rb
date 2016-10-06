f154c3f658d22390b4500ae96f3ec2e01337a6f2
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-10-06 13:10:23 +0100

    Minor code cleanup

--- app/views/components/home/clock.rb
+++ app/views/components/home/clock.rb
@@ -1,46 +1,21 @@
 module Components
-  class Clock < React::Component::Base
+  module Home
+    class Clock < React::Component::Base
+      before_mount do
+        @timer = every(1) { force_update! }
+      end
 
-    # param :my_param
-    # param param_with_default: "default value"
-    # param :param_with_default2, default: "default value" # alternative syntax
-    # param :param_with_type, type: Hash
-    # param :array_of_hashes, type: [Hash]
-    # collect_all_other_params_as :attributes  #collects all other params into a hash
-
-    # The following are the most common lifecycle call backs,
-    # the following are the most common lifecycle call backs# delete any that you are not using.
-    # call backs may also reference an instance method i.e. before_mount :my_method
-
-    before_mount do
-      @timer = every(1) { force_update! }
-      # any initialization particularly of state variables goes here.
-      # this will execute on server (prerendering) and client.
-    end
-
-    after_mount do
-      # any client only post rendering initialization goes here.
-      # i.e. start timers, HTTP requests, and low level jquery operations etc.
-    end
-
-    before_update do
-      # called whenever a component will be re-rerendered
-    end
-
-    before_unmount do
-      # cleanup any thing (i.e. timers) before component is destroyed
-    end
-
-    def render
-      div(class: "row") do
-        div(class: "col-sm-6") do
-          h1 do
-            "The current time"
+      def render
+        div(class: "row") do
+          div(class: "col-sm-6") do
+            h1 do
+              "The current time"
+            end
           end
-        end
-        div(class: "col-sm-6") do
-          strong(style: {color: "red"}) do
-            "#{Time.now}"
+          div(class: "col-sm-6") do
+            strong(style: {color: "red"}) do
+              "#{Time.now}"
+            end
           end
         end
       end
